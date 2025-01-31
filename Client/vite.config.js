521920906cab387cfb997b74b3c3a73dc81050b7
angabaes17@gmail.com <bahenaescalante.angelgabriel@utacapulco.edu.mx>
2025-01-30 20:53:03 -0600

    funciona

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps:{
-      exclude: ['blip-ds/loader']
-     },
-  })
-  
+  plugins: [react()],
+})
