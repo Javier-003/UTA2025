ceabc5de67a4f1ff1fa025e22ab3fd06884bccc3
LAPTOP-C90FPPQJ\jua20 <cruzpiza.juaneduardo@utacapulco.edu.mx>
2025-02-23 17:24:34 -0600

    Inserciones masivas de alumnotramite a alumnoproceso.

---
+++ Server/db/conexion.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+
+export const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'UT2025',
+  waitForConnections: true,
+  connectionLimit: 10, // 🔹 Permite hasta 10 conexiones simultáneas
+  queueLimit: 0
+});
+
