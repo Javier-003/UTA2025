09e95f3f1cb90fc19c29b2baa6d9589ee9f7fd55
Jared Vazquez <vazquezroman.angeljared@utacapulco.edu.mx>
2025-02-27 14:57:07 -0600

    Vista de subir calificaciones para profesor

---
+++ Client/src/pages/Profesor/SubirCalificacion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubirCalificacion = () => {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+        </div>
+    );
+};
+
+export default SubirCalificacion;
