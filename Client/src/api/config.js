6e5d5cc79e2661d29349383eda61196b434dc944
Javier-003 <javiertoledo455@mail.com>
2025-06-10 10:17:09 -0600

    Configuración de middlewares y ajustes en login, server y API

--- Client/src/api/config.js
+++ Client/src/api/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'http://192.168.1.8:3000';
+
+
--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,8 @@ import app from "./app.js";
 import {PORT} from "./conf.js"
 
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
 export default PORT;
