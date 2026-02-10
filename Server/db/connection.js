c2bdcad9fe8da8cb03da8a96604b6e9dc2b791a6
javier-003 <javiertoledo455@gmail.com>
2026-02-10 17:55:52 -0600

    config para funcionamiento local

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -4,8 +4,8 @@ import mysql from 'mysql2/promise';
 export const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-    password: '123456789',
-    database: 'UT2025', 
+    password: '123456789', // su contraseña de mysql
+    database: 'UT2025',  // el nombre de la base de datos, de preferencia creen una con el mismo nombre que aqui aparece e importan la que les mandare
   waitForConnections: true,
   connectionLimit: 10, // 🔹 Permite hasta 10 conexiones simultáneas
   queueLimit: 0
