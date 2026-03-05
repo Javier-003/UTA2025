697d3344f20fd39440d5afcab61471b0e49ab210
javier-003 <javiertoledo455@gmail.com>
2026-03-05 14:41:45 -0300

    config servidor funcionando

--- Server/index.js
+++ Server/index.js
@@ -9,10 +9,11 @@ const __dirname = path.dirname(__filename);
 // Servir frontend compilado
 app.use(express.static(path.join(__dirname, "../Client/dist")));
 
-// Catch-all SPA
-app.get("*", (req, res) => {
+// Catch-all SPA (Solo si NO es una ruta de API)
+app.get(/^(?!\/api).+/, (req, res) => {
   res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
 });
+
 
 app.listen(3000, () => { console.log("Servidor corriendo en puerto 3000 🚀"); });
 
